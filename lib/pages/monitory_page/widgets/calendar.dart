0c92bf533de87765c36b7bdaa2c23dae9e8c946f
Alejandro-Fierro <luis.fierro193@tectijuana.edu.mx>
2023-05-30 12:46:41 -0700

    calendario de monitoreo

--- lib/pages/monitory_page/widgets/calendar.dart
+++ lib/pages/monitory_page/widgets/calendar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 const List<String> mes = ['May', 'June', 'July', 'August'];
 const List<String> semana = ['1 - 7', '8 - 14', '15 - 21', '22 - 28'];
@@ -10,6 +11,7 @@ class Calendario extends StatelessWidget {
   Widget build(BuildContext context) {
     String inicio = mes.first;
     String fin = semana[3];
+    
 
     return Column(
       children: [
@@ -147,12 +149,58 @@ class Calendario extends StatelessWidget {
             ],
           ),
         ),
-        Center(
-          child: Container(
-            child: Image.asset('assets/images/diagrama.jpg'),
+        Expanded(
+          child: SfCalendar(
+            view: CalendarView.week,
+            firstDayOfWeek: 1,
+            dataSource: MeetingDataSource(getAppointments()),
+        
           ),
         ),
+          
       ],
     );
+    
+  }
+
+  
+}
+
+List<Appointment> getAppointments(){
+  List<Appointment> meet = <Appointment>[];
+  final DateTime hoy = DateTime.now();
+  final DateTime start = DateTime(hoy.year, hoy.month, hoy.day, 12, 0, 0);
+  final DateTime end = start.add(const Duration(hours: 2));
+
+  meet.add(Appointment(
+    startTime: start,
+    endTime: end,
+    subject: 'Jane Cooper\nMKS-1839',
+    color: Color.fromRGBO(191, 33, 53, 10),
+    // recurrenceRule: 'FREQ=DAILY;COUNT=10',
+    // isAllDay: true,
+  ));
+   meet.add(Appointment(
+    startTime: start,
+    endTime: end,
+    subject: 'Michael\nACT-7083',
+    color: Color.fromRGBO(52, 86, 148, 10),
+    // recurrenceRule: 'FREQ=DAILY;COUNT=10',
+    // isAllDay: true,
+  ));
+  meet.add(Appointment(
+    startTime: DateTime(hoy.year, hoy.month, hoy.day, 13, 0, 0),
+    endTime: end,
+    subject: 'Gian\nOVX-6680',
+    color: Color.fromRGBO(217, 217, 217, 10),
+    // recurrenceRule: 'FREQ=DAILY;COUNT=10',
+    // isAllDay: true,
+  ));
+
+  return meet;
+}
+class MeetingDataSource extends CalendarDataSource{
+  MeetingDataSource(List<Appointment> source){
+    appointments = source;
   }
 }
