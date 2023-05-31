e37f72dbb3b31d67c63e272aec03c30281f30596
JuanAispuro <juan.aispuro193@tectijuana.edu.mx>
2023-05-31 08:35:36 -0700

    Agregado los router y las pages faltantes

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:rta_crm_cv/pages/monitory_page/monitory_page.dart';
 
 import 'package:rta_crm_cv/pages/pages.dart';
 import 'package:rta_crm_cv/helpers/constants.dart';
 import 'package:rta_crm_cv/helpers/globals.dart';
 import 'package:rta_crm_cv/services/navigation_service.dart';
+
+import '../pages/inventory_page/inventory_page.dart';
+import '../pages/inventory_page/inventory_page_desktop.dart';
 
 /// The route configuration.
 final GoRouter router = GoRouter(
@@ -120,6 +124,24 @@ final GoRouter router = GoRouter(
       },
       pageBuilder: (context, state) =>
           pageTransition(context, state, const UsersPage()),
+    ),
+    GoRoute(
+      path: '/inventory',
+      name: 'Inventory',
+      builder: (BuildContext context, GoRouterState state) {
+        return InventoryPage();
+      },
+      pageBuilder: (context, state) =>
+          pageTransition(context, state, InventoryPage()),
+    ),
+    GoRoute(
+      path: '/network',
+      name: 'Network',
+      builder: (BuildContext context, GoRouterState state) {
+        return MonitoryPage();
+      },
+      pageBuilder: (context, state) =>
+          pageTransition(context, state, MonitoryPage()),
     ),
 
     /////////////////////////////////////////////////////////////////////////////////
