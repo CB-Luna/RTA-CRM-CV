a34496e3ebeeb0cd210a56cb50480496d9e54250
Alejandro-Fierro <luis.fierro193@tectijuana.edu.mx>
2023-06-23 13:26:00 -0700

    colores dashboard

--- lib/pages/ctrlv/dashboards_page.dart
+++ lib/pages/ctrlv/dashboards_page.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:rta_crm_cv/providers/crm/dashboard_provider.dart';
 import 'package:rta_crm_cv/providers/side_menu_provider.dart';
 import 'package:rta_crm_cv/public/colors.dart';
+import 'package:rta_crm_cv/theme/theme.dart';
 import 'package:rta_crm_cv/widgets/card_header.dart';
 import 'package:rta_crm_cv/widgets/side_menu/sidemenu.dart';
 import 'package:fl_chart/fl_chart.dart';
@@ -58,6 +59,7 @@ class _DashboardsCTRLVPageState extends State<DashboardsCTRLVPage> {
                         child: BarChart(
                           BarChartData(
                             barGroups: _getdata(),
+                            titlesData: titlesData,
                           ),
                           swapAnimationDuration:
                               Duration(milliseconds: 150), // Optional
@@ -78,38 +80,38 @@ class _DashboardsCTRLVPageState extends State<DashboardsCTRLVPage> {
   List<BarChartGroupData> _getdata() {
     List<BarChartGroupData> data = [
       BarChartGroupData(
-        x: 1,
+        x: 0,
         barRods: [
           BarChartRodData(
             toY: 10,
-            color: Colors.red,
+            color: AppTheme.of(context).secondaryColor,
           ),
           BarChartRodData(
             toY: 8,
-            color: Colors.green,
+            color: AppTheme.of(context).primaryText,
 
           ),
           BarChartRodData(
             toY: 14,
-            color: Colors.blue,
+            color: AppTheme.of(context).primaryColor,
           ),
         ],
       ),
       BarChartGroupData(
-        x: 2,
+        x: 1,
         barRods: [
           BarChartRodData(
-            toY: 10,
-            color: Colors.red,
+            toY: 25,
+            color: AppTheme.of(context).secondaryColor,
           ),
           BarChartRodData(
-            toY: 8,
-            color: Colors.green,
+            toY: 39,
+            color: AppTheme.of(context).primaryText,
 
           ),
           BarChartRodData(
-            toY: 14,
-            color: Colors.blue,
+            toY: 5,
+            color: AppTheme.of(context).primaryColor,
           ),
         ],
       )
@@ -117,4 +119,65 @@ class _DashboardsCTRLVPageState extends State<DashboardsCTRLVPage> {
 
     return data;
   }
+
+  Widget getTitles(double value, TitleMeta meta) {
+    final style = TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.bold,
+      fontSize: 14,
+    );
+    String text;
+    switch (value.toInt()) {
+      case 0:
+        text = 'Jan';
+        break;
+      case 1:
+        text = 'Feb';
+        break;
+      case 2:
+        text = 'Mar';
+        break;
+      case 3:
+        text = 'Apr';
+        break;
+      case 4:
+        text = 'May';
+        break;
+      case 5:
+        text = 'Jun';
+        break;
+      case 6:
+        text = 'Jul';
+        break;
+      default:
+        text = '';
+        break;
+    }
+    return SideTitleWidget(
+      axisSide: meta.axisSide,
+      space: 4,
+      child: Text(text, style: style),
+    );
+  }
+
+  FlTitlesData get titlesData => FlTitlesData(
+        show: true,
+        bottomTitles: AxisTitles(
+          sideTitles: SideTitles(
+            showTitles: true,
+            reservedSize: 30,
+            getTitlesWidget: getTitles,
+          ),
+        ),
+        leftTitles:  AxisTitles(
+          sideTitles: SideTitles(showTitles: true),
+        ),
+        topTitles: AxisTitles(
+          sideTitles: SideTitles(showTitles: false),
+        ),
+        rightTitles:  AxisTitles(
+          sideTitles: SideTitles(showTitles: false),
+        ),
+      );
+
 }
