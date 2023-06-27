97c0a66792ec84b8635ea119bcbb56d22e02e8b9
Uzziel Palma <uzzielpalma99@gmail.com>
2023-06-27 14:49:24 -0600

    se crea archivo de appointments cards

---
+++ lib/pages/ctrlv/monitory_page/widgets/appointment_cards.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+import 'package:rta_crm_cv/public/colors.dart';
+import 'package:rta_crm_cv/theme/theme.dart';
+import 'package:rta_crm_cv/widgets/card_header.dart';
+import 'package:rta_crm_cv/widgets/custom_scrollbar.dart';
+
+class AppointmentCards extends StatefulWidget {
+  const AppointmentCards({
+    super.key,
+    required this.title,
+    this.height,
+    this.width,
+    required this.child,
+    this.padding = const EdgeInsets.all(0),
+  });
+
+  final String title;
+  final double? height;
+  final double? width;
+  final Widget child;
+  final EdgeInsets padding;
+
+  @override
+  State<AppointmentCards> createState() => _AppointmentCardsState();
+}
+
+class _AppointmentCardsState extends State<AppointmentCards> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: widget.padding,
+      child: Container(
+        height: widget.height,
+        width: widget.width,
+        decoration: BoxDecoration(
+          border: Border.all(color: AppTheme.of(context).primaryColor, width: 2),
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(40),
+            topRight: Radius.circular(15),
+            bottomRight: Radius.circular(15),
+            bottomLeft: Radius.circular(15),
+          ),
+          gradient: whiteGradient,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            CardHeader(text: widget.title),
+            SizedBox(
+              height: widget.height != null ? widget.height! - 65 : null,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: CustomScrollBar(
+                  scrollDirection: Axis.vertical,
+                  child: widget.child,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
