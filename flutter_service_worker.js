606d1bb031b564f65e5cdc8960c29fdd783cb28d
JuanAispuro <juan.aispuro193@tectijuana.edu.mx>
2023-07-19 09:13:56 -0700

    202317190913

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -75,9 +75,9 @@ const RESOURCES = {"assets/AssetManifest.json": "34da11f8062e93539022cf85b4c3ce9
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "46060dcb1e727f1bebc3248b13acc49d",
-"/": "46060dcb1e727f1bebc3248b13acc49d",
-"main.dart.js": "6d1c3353237dd5c29730c435f1e2b660",
+"index.html": "c54cc44e34e2a5fed725d56ccf72058e",
+"/": "c54cc44e34e2a5fed725d56ccf72058e",
+"main.dart.js": "d2f37ecbaa99cd844c9ce867997b5fab",
 "manifest.json": "87360f2da4807db2d38e2ff764c9295c",
 "version.json": "c479cbf7d75e6deee99c3b920033257e"};
 // The application shell files that are downloaded before a service worker can
