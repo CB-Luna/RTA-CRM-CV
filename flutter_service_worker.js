4777c01bbd0e50e6a93b9b6ba1517693359e3080
JuanAispuro <juan.aispuro193@tectijuana.edu.mx>
2023-08-11 13:01:25 -0700

    202308111301

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -51,7 +51,7 @@ const RESOURCES = {"assets/AssetManifest.json": "34da11f8062e93539022cf85b4c3ce9
 "assets/assets/rive/tickets_icon.riv": "c3c53397031d79f5a8ede564675aa08f",
 "assets/assets/rive/users_icon.riv": "a3838de9aa7fe52c83ef6123780f2f2b",
 "assets/FontManifest.json": "5ed9ba8c3d20b6e23cce071a310ea7e4",
-"assets/fonts/MaterialIcons-Regular.otf": "ba9cc703d16dafd974373b2d55127835",
+"assets/fonts/MaterialIcons-Regular.otf": "8093acdaebd8b7aba26762e1b93d30d8",
 "assets/NOTICES": "efbba7ae92646e3142a119271e7d420d",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "57d849d738900cfd590e9adc7e208250",
 "assets/packages/flex_color_picker/assets/opacity.png": "49c4f3bcb1b25364bb4c255edcaaf5b2",
@@ -75,9 +75,9 @@ const RESOURCES = {"assets/AssetManifest.json": "34da11f8062e93539022cf85b4c3ce9
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "78fd92963f742f371d2fdd6f4b6415c2",
-"/": "78fd92963f742f371d2fdd6f4b6415c2",
-"main.dart.js": "2bf5e864dff06c46f72343e1cf4bfce2",
+"index.html": "312ad1d0d18e12e365e162e7fa729b00",
+"/": "312ad1d0d18e12e365e162e7fa729b00",
+"main.dart.js": "b3a147a802f58b3db0aa836a1101b496",
 "manifest.json": "87360f2da4807db2d38e2ff764c9295c",
 "version.json": "c479cbf7d75e6deee99c3b920033257e"};
 // The application shell files that are downloaded before a service worker can
