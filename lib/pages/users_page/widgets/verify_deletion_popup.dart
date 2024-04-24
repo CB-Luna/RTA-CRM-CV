823ae25fafae8d0fd23510c7da0347d9816cd92b
A-L-M <andreslopezmartinez0@gmail.com>
2024-04-24 13:23:34 -0600

    Cambios provider usuarios

--- lib/pages/users_page/widgets/verify_deletion_popup.dart
+++ lib/pages/users_page/widgets/verify_deletion_popup.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
+
 import 'package:rta_crm_cv/pages/ctrlv/download_apk/widgets/success_toast.dart';
 import 'package:rta_crm_cv/providers/providers.dart';
 import 'package:rta_crm_cv/theme/theme.dart';
@@ -13,8 +14,8 @@ import '../../../models/company.dart';
 import '../../../models/user.dart';
 
 class DeletePopUp extends StatefulWidget {
-  final User users;
-  const DeletePopUp({super.key, required this.users});
+  final User user;
+  const DeletePopUp({super.key, required this.user});
 
   @override
   State<DeletePopUp> createState() => _DeletePopUpState();
@@ -28,10 +29,10 @@ class _DeletePopUpState extends State<DeletePopUp> {
     fToast.init(context);
 
     UsersProvider provider = Provider.of<UsersProvider>(context);
-    List<String> listCompany = [];
-    widget.users.companies.forEach((company) {
-      listCompany.add(company.company);
-    });
+    List<String> companies = [];
+    for (var company in widget.user.companies) {
+      companies.add(company.company);
+    }
     return AlertDialog(
       backgroundColor: Colors.transparent,
       content: CustomCard(
@@ -46,8 +47,7 @@ class _DeletePopUpState extends State<DeletePopUp> {
               child: CustomTextIconButton(
                 width: MediaQuery.of(context).size.width * 0.04,
                 isLoading: false,
-                icon: Icon(Icons.arrow_back_outlined,
-                    color: AppTheme.of(context).primaryBackground),
+                icon: Icon(Icons.arrow_back_outlined, color: AppTheme.of(context).primaryBackground),
                 text: '',
                 onTap: () {
                   Navigator.pop(context);
@@ -79,8 +79,7 @@ class _DeletePopUpState extends State<DeletePopUp> {
                       ),
                       Padding(
                         padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                            " ${widget.users.name} ${widget.users.lastName} "),
+                        child: Text(" ${widget.user.name} ${widget.user.lastName} "),
                       ),
                     ],
                   ),
@@ -95,41 +94,35 @@ class _DeletePopUpState extends State<DeletePopUp> {
                       ),
                       Padding(
                         padding: const EdgeInsets.all(8.0),
-                        child: Text(" ${widget.users.sequentialId}"),
+                        child: Text(" ${widget.user.sequentialId}"),
                       ),
                     ],
                   ),
                   Padding(
                     padding: const EdgeInsets.only(right: 15),
-                    child: Container(
+                    child: SizedBox(
                       width: MediaQuery.of(context).size.width,
                       height: MediaQuery.of(context).size.height * 0.05,
                       child: ListView.builder(
                         scrollDirection: Axis.horizontal,
-                        itemCount: widget.users.companies.length,
+                        itemCount: widget.user.companies.length,
                         itemBuilder: (context, index) {
-                          Company currentCompany =
-                              widget.users.companies[index];
+                          Company currentCompany = widget.user.companies[index];
                           return Container(
                             width: MediaQuery.of(context).size.width * 0.04,
                             height: rowHeight,
                             alignment: Alignment.center,
-                            margin: const EdgeInsets.symmetric(
-                                horizontal: 2, vertical: 3),
+                            margin: const EdgeInsets.symmetric(horizontal: 2, vertical: 3),
                             decoration: BoxDecoration(
-                              color:
-                                  statusColor(currentCompany.company, context),
+                              color: statusColor(currentCompany.company, context),
                               borderRadius: BorderRadius.circular(10),
                             ),
                             child: Text(
                               currentCompany.company,
-                              style: AppTheme.of(context)
-                                  .contenidoTablas
-                                  .override(
+                              style: AppTheme.of(context).contenidoTablas.override(
                                     fontFamily: 'Gotham-Regular',
                                     useGoogleFonts: false,
-                                    color:
-                                        AppTheme.of(context).primaryBackground,
+                                    color: AppTheme.of(context).primaryBackground,
                                   ),
                             ),
                           );
@@ -137,75 +130,55 @@ class _DeletePopUpState extends State<DeletePopUp> {
                       ),
                     ),
                   ),
-                  // Padding(
-                  //   padding: const EdgeInsets.only(right: 15),
-                  //   child: Container(
-                  //     width: MediaQuery.of(context).size.width * 0.1,
-                  //     height: MediaQuery.of(context).size.height * 0.03,
-                  //     decoration: BoxDecoration(
-                  //       // color:
-                  //       //     statusColor(widget.users.company.company, context),
-                  //       color: Colors.red,
-                  //       borderRadius: BorderRadius.circular(10),
-                  //     ),
-                  //     child: Center(
-                  //       child: Text(
-                  //         "${listCompany}",
-                  //         style: const TextStyle(
-                  //           color: Colors.white,
-                  //         ),
-                  //       ),
-                  //     ),
-                  //   ),
-                  // ),
                 ],
               ),
             ),
-            Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-              CustomTextIconButton(
-                width: 96,
-                isLoading: false,
-                icon: Icon(Icons.dangerous_outlined,
-                    color: AppTheme.of(context).primaryBackground),
-                text: 'Delete',
-                color: AppTheme.of(context).secondaryColor,
-                onTap: () async {
-                  if (!mounted) return;
-                  fToast.showToast(
-                    child: const SuccessToast(
-                      message: 'User Deleted',
-                    ),
-                    gravity: ToastGravity.BOTTOM,
-                    toastDuration: const Duration(seconds: 2),
-                  );
-                  context.pop();
-                  provider.deleteUserAuth(widget.users);
-                  //provider.deleteUser(widget.users);
-                  provider.updateState();
-                },
-              ),
-              CustomTextIconButton(
-                width: 101,
-                isLoading: false,
-                icon: Icon(Icons.archive_outlined,
-                    color: AppTheme.of(context).primaryBackground),
-                text: 'Archive',
-                color: AppTheme.of(context).primaryColor,
-                onTap: () async {
-                  if (!mounted) return;
-                  fToast.showToast(
-                    child: const SuccessToast(
-                      message: 'The user has been successfully archived',
-                    ),
-                    gravity: ToastGravity.BOTTOM,
-                    toastDuration: const Duration(seconds: 2),
-                  );
-                  provider.changeStatusUser(widget.users);
-                  provider.updateState();
-                  context.pop();
-                },
-              ),
-            ]),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                CustomTextIconButton(
+                  width: 96,
+                  isLoading: false,
+                  icon: Icon(Icons.dangerous_outlined, color: AppTheme.of(context).primaryBackground),
+                  text: 'Delete',
+                  color: AppTheme.of(context).secondaryColor,
+                  onTap: () async {
+                    if (!mounted) return;
+                    fToast.showToast(
+                      child: const SuccessToast(
+                        message: 'User Deleted',
+                      ),
+                      gravity: ToastGravity.BOTTOM,
+                      toastDuration: const Duration(seconds: 2),
+                    );
+                    context.pop();
+                    provider.deleteUser(widget.user);
+                    //provider.deleteUser(widget.users);
+                    provider.updateState();
+                  },
+                ),
+                CustomTextIconButton(
+                  width: 101,
+                  isLoading: false,
+                  icon: Icon(Icons.archive_outlined, color: AppTheme.of(context).primaryBackground),
+                  text: 'Archive',
+                  color: AppTheme.of(context).primaryColor,
+                  onTap: () async {
+                    if (!mounted) return;
+                    fToast.showToast(
+                      child: const SuccessToast(
+                        message: 'The user has been successfully archived',
+                      ),
+                      gravity: ToastGravity.BOTTOM,
+                      toastDuration: const Duration(seconds: 2),
+                    );
+                    provider.changeStatusUser(widget.user);
+                    provider.updateState();
+                    context.pop();
+                  },
+                ),
+              ],
+            ),
           ],
         ),
       ),
