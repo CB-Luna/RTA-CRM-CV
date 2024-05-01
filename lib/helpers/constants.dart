7205e9471602025dbe716e6f098be4b498bc3829
Uzziel Palma <uzzielpalma99@gmail.com>
2024-05-01 10:28:51 -0600

    Se actualizan constantes

--- lib/helpers/constants.dart
+++ lib/helpers/constants.dart
@@ -3,18 +3,22 @@ import 'package:go_router/go_router.dart';
 import 'package:pluto_grid/pluto_grid.dart';
 import 'package:rta_crm_cv/theme/theme.dart';
 
-//////// PROD ////////
-final Uri urlFMTAPK = Uri.parse("https://drive.google.com/file/d/1t7K-NSZJMIlhDXZU2Zz58-w55VUo1cGP/view?usp=share_link");
-const String supabaseUrl = 'https://supa41.rtatel.com';
+//////// TEST ////////
+final Uri urlFMTAPK = Uri.parse(
+    "https://drive.google.com/file/d/1lUdS__gG-g4zSPjq_3-LFhnMOmHhYAR8/view?usp=share_link");
+const String supabaseUrl = 'https://supa42.rtatel.com';
 const String anonKey =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICAgInJvbGUiOiAiYW5vbiIsCiAgICAiaXNzIjogInN1cGFiYXNlIiwKICAgICJpYXQiOiAxNjg0ODI1MjAwLAogICAgImV4cCI6IDE4NDI2NzgwMDAKfQ.Atj9wTNbdEEVPOjstsO14DtxbY2SEpnr50elVXBgAmM';
 const redirectUrl = '$supabaseUrl/arux-change-pass/#/change-password/token';
 const themeId = String.fromEnvironment('themeId', defaultValue: '2');
 String urlNotifications = '$supabaseUrl/notifications/api';
 const String configurator = 'https://cblsrvr1.rtatel.com';
-String apiGatewayURL = "https://apps.cblsrv41.rtatel.com/wop_x2/api";
-String apiGatewayAirflowURL = "https://apps.cblsrv41.rtatel.com/wop_x2/api"; //TODO: Este no debe de existir en producción
-String powercodeURL = "https://apps.cblsrv41.rtatel.com/planbuilder/api";
+
+String enviromentDomain = 'https://apps.cblsrv41.rtatel.com';
+String apiGatewayURL = "$enviromentDomain/wop_x2/api";
+String apiGatewayAirflowURL =
+    "https://apps.cblsrv42.rtatel.com/wop_x2/api"; //TODO: Este no debe de existir en producción
+String powercodeURL = "https://apps.cblsrv42.rtatel.com/planbuilder/api";
 
 PlutoGridScrollbarConfig plutoGridScrollbarConfig(BuildContext context) {
   return PlutoGridScrollbarConfig(
