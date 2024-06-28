5a4c35f677fbb3b5e599ec39e416f004e57b3e6a
Uzziel Palma <uzzielpalma99@gmail.com>
2024-06-28 14:35:43 -0600

    Se agregan subfiltros a MAPS

--- lib/pages/dashboards_rtatel/circuits/widgets/map_lead_not_serviceable_details.dart
+++ lib/pages/dashboards_rtatel/circuits/widgets/map_lead_not_serviceable_details.dart
@@ -13,139 +13,143 @@ class LeadNotServiceableDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(15),
-      child: Card(
-        color: AppTheme.of(context).primaryBackground,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: SizedBox(
-              width: MediaQuery.of(context).size.width * 0.08,
-              child: Text(
-                mapMarker?.companyFk == 1 ? "CRY" :
-                mapMarker?.companyFk == 2 ? "ODE" :
-                mapMarker?.companyFk == 3 ? "SMI" :
-                "RTA",
-                textAlign: TextAlign.center,
-                overflow: TextOverflow.ellipsis,
-                style: AppTheme.of(context)
-                .bodyText1
-                .override(
-                  fontFamily: 'Gotham-Regular',
-                  useGoogleFonts: false,
-                  color: mapMarker?.companyFk == 1 ? AppTheme.of(context).cryPrimary :
-                  mapMarker?.companyFk == 2 ? AppTheme.of(context).odePrimary :
-                  mapMarker?.companyFk == 3 ? AppTheme.of(context).smiPrimary :
-                  AppTheme.of(context).primaryColor,
-                  fontSize: 40,
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height * 0.4,
+      child: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Card(
+          color: AppTheme.of(context).primaryBackground,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.08,
+                child: Text(
+                  mapMarker?.companyFk == 1 ? "CRY" :
+                  mapMarker?.companyFk == 2 ? "ODE" :
+                  mapMarker?.companyFk == 3 ? "SMI" :
+                  "RTA",
+                  textAlign: TextAlign.center,
+                  overflow: TextOverflow.ellipsis,
+                  style: AppTheme.of(context)
+                  .bodyText1
+                  .override(
+                    fontFamily: 'Gotham-Regular',
+                    useGoogleFonts: false,
+                    color: mapMarker?.companyFk == 1 ? AppTheme.of(context).cryPrimary :
+                    mapMarker?.companyFk == 2 ? AppTheme.of(context).odePrimary :
+                    mapMarker?.companyFk == 3 ? AppTheme.of(context).smiPrimary :
+                    AppTheme.of(context).primaryColor,
+                    fontSize: 40,
+                  ),
                 ),
               ),
             ),
-          ),
-          const SizedBox(height: 5,),
-          Text(
-            "${mapMarker?.name ?? 'Name'} ${mapMarker?.lastName ?? 'Last Name'}",
-            textAlign: TextAlign.center,
-            overflow: TextOverflow.ellipsis,
-            style: AppTheme.of(context)
-            .bodyText1
-            .override(
-              fontFamily: 'Gotham-Regular',
-              useGoogleFonts: false,
-              color: AppTheme.of(context)
-                  .primaryText,
+            const SizedBox(height: 5,),
+            Text(
+              "${mapMarker?.name ?? 'Name'} ${mapMarker?.lastName ?? 'Last Name'}",
+              textAlign: TextAlign.center,
+              overflow: TextOverflow.ellipsis,
+              style: AppTheme.of(context)
+              .bodyText1
+              .override(
+                fontFamily: 'Gotham-Regular',
+                useGoogleFonts: false,
+                color: AppTheme.of(context)
+                    .primaryText,
+              ),
             ),
-          ),
-          const SizedBox(height: 5,),
-          Text(
-            mapMarker?.email ?? "Email",
-            textAlign: TextAlign.center,
-            overflow: TextOverflow.ellipsis,
-            style: AppTheme.of(context)
-            .bodyText1
-            .override(
-              fontFamily: 'Gotham-Regular',
-              useGoogleFonts: false,
-              color: AppTheme.of(context)
-                  .primaryText,
+            const SizedBox(height: 5,),
+            Text(
+              mapMarker?.email ?? "Email",
+              textAlign: TextAlign.center,
+              overflow: TextOverflow.ellipsis,
+              style: AppTheme.of(context)
+              .bodyText1
+              .override(
+                fontFamily: 'Gotham-Regular',
+                useGoogleFonts: false,
+                color: AppTheme.of(context)
+                    .primaryText,
+              ),
             ),
-          ),
-          const SizedBox(height: 5,),
-          Text(
-            mapMarker?.address ?? "Address",
-            textAlign: TextAlign.center,
-            overflow: TextOverflow.ellipsis,
-            maxLines: 2,
-            style: AppTheme.of(context)
-            .bodyText1
-            .override(
-              fontFamily: 'Gotham-Regular',
-              useGoogleFonts: false,
-              color: AppTheme.of(context)
-                  .primaryText,
-              fontSize: 12
+            const SizedBox(height: 5,),
+            Text(
+              mapMarker?.address ?? "Address",
+              textAlign: TextAlign.center,
+              overflow: TextOverflow.ellipsis,
+              maxLines: 2,
+              style: AppTheme.of(context)
+              .bodyText1
+              .override(
+                fontFamily: 'Gotham-Regular',
+                useGoogleFonts: false,
+                color: AppTheme.of(context)
+                    .primaryText,
+                fontSize: 12
+              ),
             ),
-          ),
-          const SizedBox(height: 10,),
-          Text(
-            mapMarker?.sourceFk == 1 ? "E-commerce" :
-            mapMarker?.sourceFk == 2 ? "Facebook" :
-            "Unknown",
-            textAlign: TextAlign.center,
-            overflow: TextOverflow.ellipsis,
-            style: AppTheme.of(context)
-            .subtitle2
-            .override(
-              fontFamily: 'Gotham-Regular',
-              useGoogleFonts: false,
-              color: AppTheme.of(context)
-                  .primaryColor,
+            const SizedBox(height: 10,),
+            Text(
+              mapMarker?.sourceFk == 1 ? "E-commerce" :
+              mapMarker?.sourceFk == 2 ? "Facebook" :
+              "Unknown",
+              textAlign: TextAlign.center,
+              overflow: TextOverflow.ellipsis,
+              style: AppTheme.of(context)
+              .subtitle2
+              .override(
+                fontFamily: 'Gotham-Regular',
+                useGoogleFonts: false,
+                color: AppTheme.of(context)
+                    .primaryColor,
+              ),
             ),
-          ),
-          Text(
-            "Phone: ${mapMarker?.phone ?? 'Not selected'}",
-            textAlign: TextAlign.center,
-            overflow: TextOverflow.ellipsis,
-            style: AppTheme.of(context)
-            .subtitle2
-            .override(
-              fontFamily: 'Gotham-Regular',
-              useGoogleFonts: false,
-              color: AppTheme.of(context)
-                  .primaryText,
+            Text(
+              "Phone: ${mapMarker?.phone ?? 'Not Phone'}",
+              textAlign: TextAlign.center,
+              overflow: TextOverflow.ellipsis,
+              style: AppTheme.of(context)
+              .subtitle2
+              .override(
+                fontFamily: 'Gotham-Regular',
+                useGoogleFonts: false,
+                color: AppTheme.of(context)
+                    .primaryText,
+              ),
             ),
-          ),
-          const SizedBox(height: 10,),
-          Text(
-            "[Latitude: ${mapMarker?.latitude ?? 'Unknown'},",
-            textAlign: TextAlign.center,
-            overflow: TextOverflow.ellipsis,
-            style: AppTheme.of(context)
-            .subtitle2
-            .override(
-              fontFamily: 'Gotham-Regular',
-              useGoogleFonts: false,
-              color: AppTheme.of(context)
-                  .secondaryColor,
+            const SizedBox(height: 10,),
+            Text(
+              "[Latitude: ${mapMarker?.latitude ?? 'Unknown'},",
+              textAlign: TextAlign.center,
+              overflow: TextOverflow.ellipsis,
+              style: AppTheme.of(context)
+              .subtitle2
+              .override(
+                fontFamily: 'Gotham-Regular',
+                useGoogleFonts: false,
+                color: AppTheme.of(context)
+                    .secondaryColor,
+              ),
             ),
-          ),
-          Text(
-            "Longitude: ${mapMarker?.longitude ?? 'Unknown'}]",
-            textAlign: TextAlign.center,
-            overflow: TextOverflow.ellipsis,
-            style: AppTheme.of(context)
-            .subtitle2
-            .override(
-              fontFamily: 'Gotham-Regular',
-              useGoogleFonts: false,
-              color: AppTheme.of(context)
-                  .secondaryColor,
+            Text(
+              "Longitude: ${mapMarker?.longitude ?? 'Unknown'}]",
+              textAlign: TextAlign.center,
+              overflow: TextOverflow.ellipsis,
+              style: AppTheme.of(context)
+              .subtitle2
+              .override(
+                fontFamily: 'Gotham-Regular',
+                useGoogleFonts: false,
+                color: AppTheme.of(context)
+                    .secondaryColor,
+              ),
             ),
+            ]
           ),
-          ]
         ),
       ),
     );
